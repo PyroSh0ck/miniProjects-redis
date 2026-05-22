942d8abb8dedcfa5fece3d7f1000b6488a4a6dd5
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-22 09:45:04 -0500

    Docs: Updated docs for index.ts
    Added documentation for the file in index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+// This is the entry point for express.
+// We also allow the app to use the express json middleware
 import express from "express";
 
 const PORT = process.env.PORT || 3000;
@@ -11,5 +13,3 @@ app
   .on("error", (error) => {
     throw new Error(error.message);
   });
-
-
