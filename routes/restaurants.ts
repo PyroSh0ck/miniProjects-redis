74ee22cc5d0e0492ee8ce8adb864de8ad048281e
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-22 12:05:31 -0500

    Feat: Finished post request and connected to Redis
    Created the post request for /restaurants and created a field value pair
    in redis, with the field being a new restaurant id generated with
    nanoid, and the value being a hash of an object containing all the
    fields in the body except cuisines.

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -2,15 +2,38 @@ import express from "express";
 import { validate } from "../middlewares/validate.ts";
 import { type Restaurant, RestaurantSchema } from "../schemas/restaurant.ts";
 import { initializeRedisClient } from "../utils/client.ts";
+import { nanoid } from "nanoid";
+import { restaurantKeyById } from "../utils/keys.ts";
+import { successResponse } from "../utils/responses.ts";
 
 const router = express.Router();
 
 // Note that the post request won't go through until its been validated
-router.post("/", validate(RestaurantSchema), async (req, res) => {
+router.post("/", validate(RestaurantSchema), async (req, res, next) => {
   const data = req.body as Restaurant;
-  const client = await initializeRedisClient();
+  try {
+    const client = await initializeRedisClient();
 
-  res.send("Hello World!");
+    // This is just a lib for strong id generation
+    const id = nanoid();
+
+    // Now that we have the id, we can get the key
+    const restaurantKey = restaurantKeyById(id);
+
+    // The object that we're going to hash
+    const hashData = { id, name: data.name, location: data.location };
+
+    // Adding the restaurant key and its hash to redis field-value pair
+    // addResult will be the number of new fields added
+    const addResult = await client.hSet(restaurantKey, hashData);
+    console.log(`Number of fields added: ${addResult}`);
+
+    // Return a success (with the hash data in case you want to use it in the frontend)
+    return successResponse(res, hashData, "Added new restaurant");
+  } catch (err) {
+    // Will send the error to the next function in the middleware chain in Express
+    next(err);
+  }
 });
 
 export default router;
