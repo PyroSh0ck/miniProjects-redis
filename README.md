a7051844d896c2f4eb6669aa868b987fe3aa9bc0
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-22 09:56:07 -0500

    Feat: Added two new modules + routers
    Added the modules restaurants and cuisines inside of the routes folder,
    and exported a default express router from each of them. Then made the
    app use those for the endpoints /restaurants and /cuisines respectively,
    and added a hello world message for testing out the endpoints.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # This is a basic project that utilizes Redis, Express, and Zod
+
+This app uses modular routing, which means all the routes are in 
+separate files/modules, with each of them handling a specific
+endpoint.
--- index.ts
+++ index.ts
@@ -2,9 +2,17 @@
 // We also allow the app to use the express json middleware
 import express from "express";
 
+// Each of these modules export a express router
+import restaurantsRouter from "./routes/restaurants.ts";
+import cuisinesRouter from "./routes/cuisines.ts";
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+
+// Adding the exported routers from restaurants and cuisines to these specific endpoints
+app.use("/restaurants", restaurantsRouter);
+app.use("/cuisines", cuisinesRouter);
 
 app
   .listen(PORT, () => {
