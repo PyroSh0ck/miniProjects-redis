d15df0c6124061eee57d4fee754a6cc4bf06a6b4
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-22 10:38:07 -0500

    Feat: Added validation middleware
    Added validation.ts which checks to make sure that your body is a valid
    Zod type before handling any post request. If it isn't, then it will
    return a prettified Zod error (which may not be the most helpful, so
    change it to result.error.issues when necessary)

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { validate } from "../middlewares/validate.ts";
+import { Restaurant, RestaurantSchema } from "../schemas/restaurant.ts";
+
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+// Note that the post request won't go through until its been validated
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
   res.send("Hello World!");
 });
 
