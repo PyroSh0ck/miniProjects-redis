37cccd67269e62cf94ab17e285c6848e830d946b
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-22 11:34:45 -0500

    Feat: Added helper function for parsing keys
    Added keys.ts inside of the utils/ directory so that we can get the key
    by providing the namespaces + id

---
+++ utils/keys.ts
@@ -0,0 +1,8 @@
+// This is a helper function for getting keys for hash data
+// Most of the time, they will have prefixes (like namespaces)
+// So that we can avoid naming collisions. Like so:
+// miniProjects-redis:restaurants:restaurantId
+
+export function getKeyName(...args: string[]) {
+  return `miniProjects-redis:${args.join(":")}`;
+}
